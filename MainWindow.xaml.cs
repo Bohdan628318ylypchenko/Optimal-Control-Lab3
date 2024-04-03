ffc4d0a8f036e3e17805af6decb916a509bb293b
Bohdan Pylypchenko <bogdan314ylypchenko@gmail.com>
2024-04-04 02:04:03 +0300

    Seems that I did implement task with random search

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Optimal_Control_Lab3
 {
@@ -16,9 +7,23 @@ namespace Optimal_Control_Lab3
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random r;
+        int segmentCount = 12;
         public MainWindow()
         {
             InitializeComponent();
+            r = new Random(451);
+        }
+
+        private void Run_Click(object sender, RoutedEventArgs e)
+        {
+            double[] coordinates = new double[segmentCount + 1];
+            for (int i = 0; i < segmentCount + 1; i++)
+                coordinates[i] = r.NextDouble();
+
+            RandomSearchProblem14Solver solver = new RandomSearchProblem14Solver(r);
+            
+            var result = solver.Solve(0, Math.Pow(Math.E, 2.0), 1, 1, 12, new VN(coordinates), 0.5, 0.001, 100, 0.9);
         }
     }
 }
---
+++ VN.cs
@@ -0,0 +1,54 @@
+﻿namespace Optimal_Control_Lab3
+{
+    public class VN
+    {
+        private double[] coordinates;
+
+        public VN(double[] coordinates)
+        {
+            this.coordinates = coordinates;
+        }
+
+        public int Dimension => coordinates.Length;
+        
+        public double At(int i)
+        {
+            return coordinates[i];
+        }
+
+        public void Set(int i, double value)
+        {
+            coordinates[i] = value;
+        }
+
+        public VN MutateByNormalize()
+        {
+            var module = Math.Sqrt(coordinates.Select(c => c * c).Sum());
+            for (int i = 0; i < coordinates.Length; i++)
+                coordinates[i] /= module;
+            return this;
+        }
+
+        public VN MutateByAdd(VN other)
+        {
+            if (coordinates.Length == other.coordinates.Length)
+                for (int i = 0; i < coordinates.Length; i++)
+                    coordinates[i] += other.coordinates[i];
+            return this;
+        }
+
+        public VN MutateByMultiplyOnScalar(double k)
+        {
+            for (int i = 0; i < coordinates.Length; i++)
+                coordinates[i] *= k;
+            return this;
+        }
+
+        public VN DeepCopy()
+        {
+            double[] cloneCoordinates = new double[this.coordinates.Length];
+            Array.Copy(this.coordinates, cloneCoordinates, this.coordinates.Length);
+            return new VN(cloneCoordinates);
+        }
+    }
+}
